ec1f78d763628e8a27541d42a1aad4b6d46d3c9d
zheka <zheka@HOME-PC>
2024-10-03 18:21:24 +0300

    HomeWork2

---
+++ DataTypes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n-----------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello DataTypes");
+			/*	Console.WriteLine(sizeof(bool));
+				Console.WriteLine(true);
+				Console.WriteLine(false);
+				Console.WriteLine(Boolean.FalseString);*/
+
+			//Console.WriteLine(sizeof(char));   //Unicode
+			//Console.WriteLine('+'.GetType());
+			/*
+            --------------------------------------
+			              unsigned:
+			  byte;        sbyte;    1 byte
+			  short;       ushort;   2 byte
+			  int;         uint;     4 byte
+			  long;        ulong;    8 byte
+			--------------------------------------                                    */
+			Console.WriteLine("Byte: ");
+			Console.WriteLine(sizeof(byte));
+			Console.WriteLine(byte.MinValue);
+			Console.WriteLine(byte.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("SByte: ");
+			Console.WriteLine(sizeof(sbyte));
+			Console.WriteLine(sbyte.MinValue);
+			Console.WriteLine(sbyte.MaxValue);
+
+			double a, b;
+			char s;
+			while (true)
+			{	
+					Console.Write("Введите выражение через ENTER: ");
+					a = double.Parse(Console.ReadLine());
+					s = char.Parse(Console.ReadLine());
+					b = double.Parse(Console.ReadLine());
+					switch (s)
+					{
+						case '+': Console.WriteLine(a + b); break;
+						case '-': Console.WriteLine(a - b); break;
+						case '*': Console.WriteLine(a * b); break;
+						case '/': Console.WriteLine(a / b); break;
+						default:
+						Console.WriteLine("Ошибка, попробуйте снова!");
+						continue;
+				    }
+			}
+		}
+	}
+}
